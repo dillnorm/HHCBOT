efb8b0a7f2e5a9085ca5abc6ba6acc5b2ce0f3a6
dillnorm <31057595+dillnorm@users.noreply.github.com>
2019-10-15 15:54:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ var botID = process.env.BOT_ID;
     // catch any errors
   });
 }*/
-function weatherBalloon() {
+/*function weatherBalloon() {
   var cityID = "Statesboro,GA";
   var key = '{9403c5022e4b2d80a2bcc5739332adff}';
   axios.get('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)
@@ -25,6 +25,30 @@ function weatherBalloon() {
   .catch(function (error) {
     console.log(error);
    });
+}
+*/
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") return;
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
+function weatherBalloon() {
+    var urlString = "https://api.openweathermap.org/data/2.5/weather?id=" + cityID+ "&appid=" + key;
+    urlParams = parseURLParams(urlString);
 }
 
 function respond() {
