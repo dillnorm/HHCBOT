c9d87c8f8c3b07b07eaadc1365aad01848ea10fa
dillnorm <31057595+dillnorm@users.noreply.github.com>
2019-10-15 15:56:06 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,6 +47,8 @@ function parseURLParams(url) {
     return parms;
 }
 function weatherBalloon() {
+    var cityID = "Statesboro,GA";
+    var key = '{9403c5022e4b2d80a2bcc5739332adff}';
     var urlString = "https://api.openweathermap.org/data/2.5/weather?id=" + cityID+ "&appid=" + key;
     urlParams = parseURLParams(urlString);
 }
